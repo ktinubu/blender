1a8dd57aeaeb7862595d6fd748ec9806a461cecc
Campbell Barton <ideasman42@gmail.com>
2017-03-16 06:57:44 +1100

    Run template registration on startup (without explicitly selecting)

--- release/scripts/modules/bpy/utils/__init__.py
+++ release/scripts/modules/bpy/utils/__init__.py
@@ -68,6 +68,7 @@ import os as _os
 import sys as _sys
 
 import addon_utils as _addon_utils
+import app_template_utils as _app_template_utils
 
 _user_preferences = _bpy.context.user_preferences
 _script_module_dirs = "startup", "modules"
@@ -245,6 +246,9 @@ def load_scripts(reload_scripts=False, refresh_scripts=False):
 
                     for mod in modules_from_path(path, loaded_modules):
                         test_register(mod)
+
+    # load template (if set)
+    _app_template_utils.reset(reload_scripts=reload_scripts)
 
     # deal with addons separately
     _initialize = getattr(_addon_utils, "_initialize", None)
